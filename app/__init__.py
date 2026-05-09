b6de271370d105afb4de6c2da19c975ef1eb39c4
tardyblaque0404 <cliffjonye@gmail.com>
2026-05-09 15:44:04 +0300

    fix cors for vercel

--- app/__init__.py
+++ app/__init__.py
@@ -1,26 +1,29 @@
-from flask import Flask
+from flask import Flask, app
 from flask_sqlalchemy import SQLAlchemy
 from flask_jwt_extended import JWTManager # Added this
 from flask_cors import CORS # Highly recommended for React/Flask setups
 from config.config import config
 
 db = SQLAlchemy()
-jwt = JWTManager() # Added this
+jwt = JWTManager() 
 
 def create_app(config_name='default'):
     app = Flask(__name__)
     app.config.from_object(config[config_name])
 
-    # Ensure JWT_SECRET_KEY exists, fallback to SECRET_KEY if not
+    
     if not app.config.get('JWT_SECRET_KEY'):
         app.config['JWT_SECRET_KEY'] = app.config.get('SECRET_KEY', 'fallback-very-secret-key')
 
-    # Init extensions
-    db.init_app(app)
-    jwt.init_app(app) # Added this
     
-    # Enable CORS so your React Vite app can talk to Flask without port errors
-    CORS(app)
+    db.init_app(app)
+    jwt.init_app(app) 
+    
+    
+    CORS(app, supports_credentials=True, origins=[
+    "http://localhost:5173",
+    "https://miliki-kasri.vercel.app"
+   ])
 
     # Register blueprints
     from app.routes.auth import auth_bp
