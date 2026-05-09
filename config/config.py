1a6491b5943f1f518e869b081af7a36478d93646
tardyblaque0404 <cliffjonye@gmail.com>
2026-05-09 13:24:21 +0300

    fix config dictionary

--- config/config.py
+++ config/config.py
@@ -4,6 +4,16 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY', 'miliki-kasri-dev-key')
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'sqlite:///dev.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SESSION_TYPE = 'filesystem'
-    SESSION_PERMANENT = False
-    SESSION_USE_SIGNER = True
+    JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY', 'miliki-kasri-jwt-secret')
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+
+class ProductionConfig(Config):
+    DEBUG = False
+
+config = {
+    'development': DevelopmentConfig,
+    'production':  ProductionConfig,
+    'default':     DevelopmentConfig
+}
